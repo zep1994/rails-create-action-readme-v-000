a112669d9bf9ac7c5df6ddfb6fcbe10cdf83e0bd
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-09-22 15:30:39 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,5 +11,12 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def create
+    @post = Post.new
+    @post.title = params[:title]
+    @post.description = params[:description]
+    @post.save
+    redirect_to post_path(@post)
+  end
   # add create method here
 end
